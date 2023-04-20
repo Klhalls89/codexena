dfdeb1a77779b39010475dfeacc94f12fbfdc032
Kristen Hallstrom <klhalls89@gmail.com>
2023-04-20 16:48:50 -0600

    add about and resource pages

--- src/About.js
+++ src/About.js
@@ -1,10 +1,28 @@
 import Nav from './Nav.js'
+import './About.css'
+import profile from './assets/profile.jpg'
 
 function About() {
   return (
     <div className="App">
       <Nav />
-      <h2>About</h2>
+      <div className="mainDiv">
+        <main>
+          <h2><span className="pink">About M</span>e</h2>
+          <div className="pDiv">
+            <img src={profile}/>
+            <p>My name is Kristen. I began my software journey in 2019 after
+            graduating the Turing School of Software & Design. Turing's
+            Front-End Engineering certification was so challanging and life
+            changing. Previous I had done graphic design and customer
+            service. I decided I wanted something more challenging with more
+            career options.
+            </p>
+            <p>
+            </p>
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
--- src/Resources.js
+++ src/Resources.js
@@ -1,10 +1,20 @@
 import Nav from './Nav.js'
+import './Resources.css'
+import YouTube from 'react-youtube';
 
 function Resources() {
   return (
     <div className="App">
       <Nav />
-      <h2>Resources</h2>
+      <div className="mainDiv">
+        <main>
+          <h2><span className="pink">Resource</span>s</h2>
+          <div className="videoDiv">
+            <p>Videos</p>
+            <YouTube videoId="OD6zgzd2NQc" />
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
